ddb90a7bea2567d809ff61a82126b6a3264a8c76
Nate Hofmann <inatejh@gmail.com>
2019-12-26 22:55:31 -0600

    java 11 installed, portfolio interface

---
+++ src/Portfolio.java
@@ -0,0 +1,24 @@
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Represents a potential portfolio of assets and their respective weights in that portfolio.
+ */
+public interface Portfolio {
+
+  /**
+   * Returns the raw representation of this portfolio as array of N doubles, where N is the number
+   * of assets being considered to make up the portfolio (size of the universe of available assets).
+   * ith item represents the weight of the associated asset. All weights are greater than zero,
+   * weight of zero means portfolio does not include the associated asset. All weights should sum
+   * to around 1 (within some tolerance threshold).
+   * @return array of doubles representing the weights currently assigned to each associated asset
+   */
+  double[] getRawRepresentation();
+
+  /**
+   * Returns a mapping of assets to current portfolio weights, for all non-zero assets.
+   * @return mapping of assets to current portfolio weights
+   */
+  Map<String, Double> assetsToWeights();
+}
